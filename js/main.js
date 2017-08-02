448228d5905721dd3321da6d168494fcecb90f4b
Kimi W <kimimoanawhiting@gmail.com>
2017-08-03 11:00:04 +1200

    comic sans ...

--- js/main.js
+++ js/main.js
@@ -39,3 +39,11 @@ function menuClick() {
 }
 
 menuClick();
+
+define(function (require, exports, module) {
+    "use strict";
+
+    var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+
+    ExtensionUtils.addEmbeddedStyleSheet(".CodeMirror { font-family: 'Comic Sans MS'; }");
+});
